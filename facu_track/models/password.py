ec88f356c34770efaea074d5326ea302f3a28c71
root <root@vmi1430464.contaboserver.net>
2023-11-15 15:05:15 +0100

    added

---
+++ facu_track/models/password.py
@@ -0,0 +1,10 @@
+from odoo import fields, models, api,_
+from odoo.exceptions import ValidationError
+
+class Paasword(models.TransientModel):
+    _name = 'facu_track.password'
+
+    new_password = fields.Char(string="New Password")
+    old_password = fields.Char(string="Old Password")
+    def change_password(self):
+        self.env.user.password = self.new_password
